5e4974727300bac32f170439468466923124532f
Björn-Eric <bjorn.umera@gmail.com>
2023-08-24 16:37:03 +0300

    Installed package for font awesome icons and added some text and styling

--- new-app/App.js
+++ new-app/App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {FontAwesomeIcon} from "@fortawesome/react-native-fontawesome";
+import {faHeart} from "@fortawesome/free-solid-svg-icons";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Testi sovellus joka toimii!</Text>
+      <FontAwesomeIcon icon={faHeart} style={styles.icon} size={50} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: '#ffffff',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  icon: {
+    color: '#fb0404',
+    marginTop: 40,
+  }
 });

57616579e0636fce3d8ee81c08ec113219f6d80d
Björn-Eric <bjorn.umera@gmail.com>
2023-08-24 18:15:50 +0300

    Created the app

--- laskin/App.js
+++ laskin/App.js
@@ -1,10 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
+import {useState} from "react";
 
 export default function App() {
+
+    //TODO: Create a unified state. Do not use multiple states for each number.
+    const [resultText, setResultText] = useState("");
+    const [firstNumber, setFirstNumber] = useState(null);
+    const [secondNumber, setSecondNumber] = useState(null);
+
+    const onCalculatePress = (operator) => {
+        if(firstNumber === null || secondNumber === null) {
+            alert("Syötä molemmat numerot!");
+            return;
+        }
+
+        if(operator === "+") {
+            setResultText(parseFloat(firstNumber) + parseFloat(secondNumber));
+        }else {
+            setResultText(parseFloat(firstNumber) - parseFloat(secondNumber));
+        }
+    }
+
+    const onClearPress = () => {
+        setResultText("");
+        setFirstNumber(null);
+        setSecondNumber(null);
+    }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.answerText}>Vastaus: {resultText}</Text>
+        <TextInput
+            style={styles.textInput}
+            onChangeText={setFirstNumber}
+            keyboardType='number-pad'
+            value={firstNumber}
+            placeholder="Syötä numero"
+        />
+        <TextInput
+            style={styles.textInput}
+            onChangeText={setSecondNumber}
+            value={secondNumber}
+            keyboardType='number-pad'
+            placeholder="Syötä nsumero"
+        />
+      <View style={styles.buttonContainer}>
+        <Button title="+" onPress={() => onCalculatePress("+")}/>
+        <Button title="-" onPress={ ()=> onCalculatePress("-")}/>
+      </View>
+      <Button title={"Tyhjennä"} onPress={onClearPress}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +62,24 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textInputCotainer: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  textInput: {
+    borderBottomWidth: 1,
+    margin: 10,
+    borderBottomColor: 'grey',
+    width: 200,
+    height: 35,
+  },
+    buttonContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      width: 200,
+    },
+    answerText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    }
 });
